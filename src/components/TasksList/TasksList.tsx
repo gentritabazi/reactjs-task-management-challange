7540d2b404b8150c7f17feddad1ddb753fb0f6fc
Gentrit Abazi <gentritabazi01@gmail.com>
2023-05-24 18:04:32 +0200

    Create TasksList component

---
+++ src/components/TasksList/TasksList.tsx
@@ -0,0 +1,33 @@
+import { useContext } from "react";
+import { MainContext } from "../../contexts/MainContext";
+import { Grid } from "@mui/material";
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
+
+const TasksList = () => {
+  const { todos } = useContext(MainContext)!;
+
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  }));
+
+  return (
+    <div>
+      <Grid container spacing={2}>
+        {todos.map((todo) => {
+          return (
+            <Grid item xs={6} key={todo.id}>
+              <Item>{todo.title}</Item>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </div>
+  );
+};
+
+export default TasksList;
