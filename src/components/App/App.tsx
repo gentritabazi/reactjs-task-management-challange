fedd3297885c02897c66e63223b6bd2247f519c4
Gentrit Abazi <gentritabazi01@gmail.com>
2023-05-24 18:04:56 +0200

    Include home in App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,13 @@
 import "../../assets/css/main.css";
-import ButtonAppBar from "../ButtonAppBar";
+import Home from "../../pages/Home/Home";
+import AppLayout from "../../layouts/AppLayout/AppLayout";
 
 const App: React.FC = () => {
   return (
     <div id="App">
-      <ButtonAppBar />
+      <AppLayout>
+        <Home />
+      </AppLayout>
     </div>
   );
 };
